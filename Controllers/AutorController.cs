7430bdf1d91e776a977ed425c2896b0c78da03f8
msejuro312 <msejuro312@protonmail.com>
2026-08-02 09:11:44 -0500

    02.08.2026 consumo servicios inicio

---
+++ Controllers/AutorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AutorController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string apiBase = "https://localhost:7092";
+
+        public AutorController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(apiBase);
+        }
+
+        
+    }
+}
---
+++ Models/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class ApiResponse
+    {
+        public string message { get; set; }
+        public bool success { get; set; }
+        public T data { get; set; }
+    }
+}
